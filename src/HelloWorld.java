98e238ed56e8b2b466c6c296afed7a9eea606436
Dominik Zydak <dominox71@wp.pl>
2020-10-13 16:17:21 +0200

    commit commit

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
@@ -8,6 +10,21 @@ public class HelloWorld {
         String string= "ciekawe_rzeczy";
         System.out.println(a);
         System.out.println(string);
+        Scanner sc = new Scanner(System.in);
+        System.out.println("podaj imie");
+        String line= sc.nextLine();
+        System.out.println("Wyświetlam:" + line);
+        System.out.println("podaj wiek");
+        line = sc.nextLine();
+        int wiek = Integer.parseInt(line);
+        System.out.println(wiek);
+        line = sc.nextLine();
+        boolean boo = Boolean.parseBoolean(line);
+        System.out.println(boo);
+        boolean val = Boolean.parseBoolean(sc.nextLine());
+        line = sc.nextLine();
+        double dd = Double.parseDouble(line);
+        System.out.println(dd);
 
     }
 }
