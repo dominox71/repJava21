5089e99450682b15eb209360e1456f780827b64a
Dominik Zydak <dominox71@wp.pl>
2020-10-13 15:18:57 +0200

    commit commit

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,10 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        int a=1;
+        String string= "ciekawe_rzeczy";
+        System.out.println(a);
+        System.out.println(string);
+
     }
 }
