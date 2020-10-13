af96ee5b03a69e5d2665a24bece52b5fa619e468
Dominik Zydak <dominox71@wp.pl>
2020-10-13 15:24:19 +0200

    commit commit

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,6 +2,9 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
         int a=1;
+        float b = 3.1F;
+        float c = a+b;
+        System.out.println(c);
         String string= "ciekawe_rzeczy";
         System.out.println(a);
         System.out.println(string);
